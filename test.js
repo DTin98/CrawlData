91a0548a2dc3273794793390fc650ffb352e230a
DTin98 <kaisin1505@gmail.com>
2020-12-04 22:27:00 +0700

    change to 'process'

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log(process.argv.slice(2));
+const { spawn } = require("child_process");
+
+const child = spawn("find", ["."]);
+
+child.stdout.on("data", (data) => {
+  console.log(`stdout:\n${data}`);
+});
+
+child.stderr.on("data", (data) => {
+  console.error(`stderr: ${data}`);
+});
